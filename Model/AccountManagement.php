31e4e9c427d7b6c139ca4c08ceea879ec3c45869
Bitixel <info@bitixel.com>
2018-05-31 01:09:17 +0530

    update error message

--- Model/AccountManagement.php
+++ Model/AccountManagement.php
@@ -2,35 +2,18 @@
 namespace Bitixel\RegistrationValidation\Model;
 
 use Magento\Framework\Exception\LocalizedException;
+use Magento\Customer\Api\Data\CustomerInterface;
 
-class AccountManagement extends Magento\Customer\Model\AccountManagement
+class AccountManagement extends \Magento\Customer\Model\AccountManagement
 {
 
-    /**
-     * {@inheritdoc}
-     */
     public function createAccount(CustomerInterface $customer, $password = null, $redirectUrl = '')
     {
-    
-    
     	$customerEmail = $customer->getEmail();
 	if (strpos($customerEmail, 'mail.ru') !== false) {
-		throw new LocalizedException(__('Error: x589'));
-	}
-    
-        if ($password !== null) {
-            $this->checkPasswordStrength($password);
-            $customerEmail = $customer->getEmail();
-            try {
-                $this->credentialsValidator->checkPasswordDifferentFromEmail($customerEmail, $password);
-            } catch (InputException $e) {
-                throw new LocalizedException(__('Password cannot be the same as email address.'));
-            }
-            $hash = $this->createPasswordHash($password);
-        } else {
-            $hash = null;
-        }
-        return $this->createAccountWithPasswordHash($customer, $hash, $redirectUrl);
+		throw new LocalizedException(__('Sorry, your email address has been blacklisted'));
+	}	    	
+        return parent::createAccount($customer, $password, $redirectUrl);
     }
 
 }

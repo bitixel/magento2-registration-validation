1d879a6d6ed1d230f8332d8501d3081bee07c9d9
bitixel <info@bitixel.com>
2018-07-06 20:41:28 +0000

    validate name

--- Model/AccountManagement.php
+++ Model/AccountManagement.php
@@ -9,10 +9,22 @@ class AccountManagement extends \Magento\Customer\Model\AccountManagement
 
     public function createAccount(CustomerInterface $customer, $password = null, $redirectUrl = '')
     {
-    	$customerEmail = $customer->getEmail();
-	if (strpos($customerEmail, 'mail.ru') !== false) {
-		throw new LocalizedException(__('Sorry, your email address has been blacklisted'));
-	}	    	
+
+        $customerEmail = $customer->getEmail();
+        $customerLastname = $customer->getLastname();
+        $customerFirstname = $customer->getFirstname();
+        
+        if(strlen($customerFirstname)>64 || strlen($customerLastname)>64){
+            throw new LocalizedException(__('Sorry, please enter valid name'));
+        }
+
+        if (strpos($customerFirstname, 'http') !== false | strpos($customerLastname, 'http') !== false) {
+            throw new LocalizedException(__('Sorry, please enter valid name'));
+        }
+
+        if (strpos($customerEmail, 'mail.ru') !== false) {
+            throw new LocalizedException(__('Sorry, your email address has been blacklisted'));
+        }	    	
         return parent::createAccount($customer, $password, $redirectUrl);
     }
 
